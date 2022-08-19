aa5ba92f3ff3772593f34cf9661237f11fb2a36d
Wilson Chen <wilsonchen901@gmail.com>
2022-08-18 22:12:29 -0400

    start movie buff homework

--- W5D5/practice-for-ch-sql-movie-buff-in-training/movie_novice/queries.rb
+++ W5D5/practice-for-ch-sql-movie-buff-in-training/movie_novice/queries.rb
@@ -30,19 +30,19 @@
 
 def find_angelina
   # find Angelina Jolie by name in the actors table
-
+  Actor.find_by(name: "Angelina Jolie")
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-  
+  Movie.select('title').where("score >= 9")
 end
 
 def star_wars
   # display the id, title, and year of each Star Wars movie in movies
   # hint: use 'select' and 'where'
-  
+  Movie.select('id', 'title', 'yr').where("title LIKE 'Star Wars%'")
 end
 
 
@@ -50,14 +50,14 @@ def below_average_years
   # display each year with movies scoring under 5, with the count of movies
   # scoring under 5 aliased as bad_movies, in descending order
   # hint: use 'select', 'where', 'group', 'order'
-  
+  Movie.select('COUNT(score < 5) AS bad_movies', 'yr').where("score < 5").group("yr").order("bad_movies DESC")
 end
 
 def alphabetized_actors
   # display the id and name of the second 10 actor names (i.e., #s 11-20)
   # when ordered from A-Z
   # hint: use 'order' and 'limit'
-  
+  Actor.select('id', 'name').order("name DESC").offset(10)
 end
 
 def pulp_fiction_actors
