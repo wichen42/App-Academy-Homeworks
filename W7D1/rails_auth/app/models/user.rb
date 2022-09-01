9f0e56ffaba9f2e93192b4e65066f50ff74f4999
Wilson Chen <wilsonchen901@gmail.com>
2022-09-01 09:33:37 -0400

    create Week 7 Day 4

---
+++ W7D1/rails_auth/app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+    attr_reader :password
+
+    before_validation :ensure_session_token
+
+    validates :username, :session_token, presence: true
+    validates :password_digest, presence: {message: 'Password cannot be blank.'}
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end 
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end 
+
+end
--- W7D2/practice-for-ch-rails-helpers-layouts-mailers/app/mailers/user_mailer.rb
+++ W7D2/practice-for-ch-rails-helpers-layouts-mailers/app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
     def welcome_user(user)
         @user = user
-        
+        @url = 'http://google.com'
+        mail(to: user.username, subject: 'Welcome to my website!')
     end
 end
