0d2b749090c60a6412b0afa5356bb3666bef843e
Wilson Chen <wilsonchen901@gmail.com>
2022-09-25 18:13:18 -0400

    create useEffect folder

--- W11D1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ W11D1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -1,7 +1,35 @@
+import './UseState.css';
+import { useState } from 'react';
+
 const UseState = () => {
+
+  const [theme, setTheme] = useState('');
+  let [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1)
+  }
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1)
+  }
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
+
   return (
     <div>
-      <h1>UseState Component</h1>
+      <h1 className={theme}>UseState Component</h1>
+      <button onClick={() => setTheme('dark')}>Dark</button>
+      <button onClick={() => setTheme('light')}>Light</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <p>{count}</p>
     </div>
   );
 };
