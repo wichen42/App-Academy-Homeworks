59e41f453fa3129bccec906f833dc4c967385535
Wilson Chen <wilsonchen901@gmail.com>
2022-09-25 20:48:18 +0000

    Update 2 files

--- W11D1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ W11D1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -1,7 +1,11 @@
+import './UseState.css';
+import {UseState} from 'react';
+
 const UseState = () => {
   return (
     <div>
       <h1>UseState Component</h1>
+      console.log(useState('light'))
     </div>
   );
 };
